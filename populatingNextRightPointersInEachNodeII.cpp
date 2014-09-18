cc0b2f0100bfce739eedbb3b418d0f233db867fb
fzzying <fzzying@outlook.com>
2014-09-18 01:23:21 -0700

    populatingNextRightPointersInEachNodeII

--- populatingNextRightPointersInEachNodeII.cpp
+++ populatingNextRightPointersInEachNodeII.cpp
@@ -11,39 +11,40 @@ struct TreeLinkNode{
 class Solution{
     public:
         void connect(TreeLinkNode* root){
-            if(root==NULL) return;
-            if(root->left==NULL && root->right==NULL) return;
             TreeLinkNode* head = root;
             while(head!=NULL){
-                TreeLinkNode *LastNode,*nextNode=head->next;
-                if(head->left!=NULL){
-                    if(head->right!=NULL){
-                        head->left->next=head->right; 
-                        LastNode=head->right;
-                    }else{
-                        LastNode=head->left; 
-                    }
-                    head=head->left;
-                }else{
-                    LastNode=head->right; 
-                    head=head->right;
-                }
-                while(nextNode!=NULL){
-                    if(nextNode->left!=NULL){
-                        LastNode->next=nextNode->left; 
-                        if(nextNode->right!=NULL){
-                            nextNode->left->next=nextNode->right; 
-                            LastNode = nextNode->right;
-                        }else{
-                            LastNode = nextNode->left; 
-                        }
-                    }else
-                    if(LastNode!=NULL)
-                    {
-                        LastNode->next=nextNode->right; 
-                        LastNode=nextNode->right;
-                    }
+                TreeLinkNode *LastNode=NULL, *nextNode=head;
+                while(nextNode!=NULL&&nextNode->left==NULL&&nextNode->right==NULL){
                     nextNode=nextNode->next; 
+                }
+                if(nextNode==NULL){
+                    head=NULL;   
+                }else{
+                    head=nextNode->left!=NULL?nextNode->left:nextNode->right; 
+                    while(nextNode!=NULL){
+                        if(nextNode->left!=NULL||nextNode->right!=NULL){
+                            if(nextNode->left!=NULL&&nextNode->right!=NULL){
+                                nextNode->left->next=nextNode->right;
+                                if(LastNode!=NULL) {
+                                    LastNode->next=nextNode->left;
+                                }
+                                LastNode=nextNode->right; 
+                            }
+                            if(nextNode->left==NULL&&nextNode->right!=NULL){
+                                if(LastNode!=NULL) {
+                                    LastNode->next=nextNode->right;
+                                }
+                                LastNode=nextNode->right; 
+                            }
+                            if(nextNode->left!=NULL&&nextNode->right==NULL){
+                                if(LastNode!=NULL) {
+                                    LastNode->next=nextNode->left;
+                                }
+                                LastNode=nextNode->left; 
+                            }
+                        }
+                        nextNode=nextNode->next;
+                    }
                 }
             }
         }
