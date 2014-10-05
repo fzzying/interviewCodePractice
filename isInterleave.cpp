15d673d5864c66dfcd140331a78945a081c66fb3
fzzying <fzzying@outlook.com>
2014-10-04 22:50:20 -0700

    Update isInterleave.cpp

--- isInterleave.cpp
+++ isInterleave.cpp
@@ -5,30 +5,24 @@ using namespace std;
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
+        s1.insert(s1.begin(),'$');
+        s2.insert(s2.begin(),'$');
+        s3.insert(s3.begin(),'$');
         int size1 = s1.length();
         int size2 = s2.length();
         int size3 = s3.length();
-        if(size1+size2!=size3) return false;
-        if(size1==0||size2==0||size3==0)
-        {
-            if(size1==0&&size2==0&&size3==0){
-                return true;
-            }
-            if(size3==0){
-                return false;
-            }
-            return s3.compare(s1)==0 || s3.compare(s2)==0;
-        }
+        if(size1+size2-2!=size3-1) return false;
         bool X[2][size2];
         for(int i=0;i<size2;i++){
             X[0][i]=true;
             X[1][i]=false;
         }
+        X[1][0]=true;
         int id = 1;
-        for(int i=0;i<size3;i++){
+        for(int i=1;i<size3;i++){
             int size = min(size2,i+1);
-            for(int j=0;j<size;j++){
-                  X[id][j] = (X[1-id][j] && s3[i]==s1[i-j]) || (j>=1 && X[1-id][j-1] && s3[i]==s2[j])||(j==0 && s3[i]==s2[j]);
+            for(int j=1;j<size;j++){
+                  X[id][j] = (X[1-id][j] && s3[i]==s1[i-j]) || (X[1-id][j-1] && s3[i]==s2[j]);
             }
             id = id ^ 1;
         }
